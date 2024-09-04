f5967781506a4d3bfb7d69ab7b1b763e8b0be98b
Lauraocampor <laura_ocampo5@hotmail.com>
2024-09-03 21:14:15 -0400

    Action added at index

--- api/index.tsx
+++ api/index.tsx
@@ -36,7 +36,7 @@ export const app = new Frog({
 app.frame('/', (c) => {
   
   return c.res({
-/*     action:  '/delegatesStats', */
+    action:  '/delegatesStats',
     image: `/Frame_1_start.png`,
     imageAspectRatio: '1.91:1',
     intents: [
