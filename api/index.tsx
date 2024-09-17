26593ba918ffd44aa972c430cebc146c55543138
Laura Ocampo Rodríguez <92342585+Lauraocampor@users.noreply.github.com>
2024-09-17 14:58:46 -0400

    testing frontend

--- api/index.tsx
+++ api/index.tsx
@@ -1,11 +1,10 @@
 import * as dotenv from 'dotenv'
 dotenv.config();
 
-import { Button, Frog, FrameIntent } from 'frog'
+import { Button, Frog, FrameIntent} from 'frog'
 import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
 import { devtools } from 'frog/dev'
-import { postComposerCreateCastActionMessage } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
 
 import { DelegatesResponseDTO } from './service/delegatesResponseDTO.js';
@@ -13,7 +12,7 @@ import { addressCount, suggestionResponseDTO } from './service/suggestionRespons
 
 // Uncomment to use Edge Runtime.
 // export const config = {
-//   runtime: 'edge',
+// runtime: 'edge',
 // }
 
 export const app = new Frog({
@@ -44,12 +43,12 @@ app.frame('/', (c) => {
 })
 
 app.frame('/delegatesStats', async (c) => {
- /*  const {  frameData } = c;
- const { fid, url } = frameData || {}    */
+ /* const {  frameData } = c;
+ const { fid } = frameData || {} */
 
  const fid = 192336
 
- if (fid === undefined){
+ if (typeof fid !== 'number' || fid === null){
   return c.res({
     image: `/Frame_6_error.png`,
     imageAspectRatio: '1.91:1',
@@ -121,10 +120,11 @@ try {
   const addressDelegate = delegate.delegateInfo.delegateAddress
 
   const delegateData = userDelegate? userDelegate : addressDelegate
+  const delegateUpperCase= delegateData.toUpperCase()
   if(!delegate.isGoodDelegate) {
 
     return c.res({
-        image: (          
+        image: (
           <div style={{
             display: 'flex',
             background: '#f6f6f6',
@@ -134,29 +134,34 @@ try {
             justifyContent: 'flex-end',
             alignItems: 'center',
             position: 'relative'
-            }}>
-            <img width="1200" height="630" alt="background" src={`/Frame_2.1_bad_delegate_stats.png`}/>
+          }}>
+            {/* @ts-ignore */}
+            <img width="1200" height="630" alt="background" src={`/Frame_2.1_bad_delegate_stats_dynamic.png`} style={{position: 'absolute', width: '100%', height: '100%', objectFit: 'cover'}} />
             <div
               style={{
+                display: 'flex',
                 position: 'absolute',
-                color: '#E5383B',
-                fontSize: '75px',
-                lineHeight: '0.7',
-                textTransform: 'uppercase',
+                color: '#000000', 
+                fontSize: '75px', 
+                fontWeight: 'bold', 
+                lineHeight: '0.7', 
+                textTransform: 'uppercase', 
                 letterSpacing: '-0.030em',
                 whiteSpace: 'wrap',
                 overflow: 'hidden',
                 textOverflow: 'ellipsis',
-                with: '100%',
-                maxWidth: '255px',
-                height: '100%',
-                maxHeight: '340px',
-                left: '190px',
-                bottom: '230px',
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-              }}>
-              {`${delegateData}`}
+                width: '100%', 
+                margin: '20px', 
+                paddingLeft: '25px',
+                paddingRight: '25px',
+                left: '-20px', 
+                top: '20px', 
+                textAlign: 'center'
+              }}
+            >            
+              {`Did `}
+              <span style={{ color: '#E5383B' }}>{delegateUpperCase}</span>
+              <span style={{ width: '100%', wordWrap: 'break-word', whiteSpace: 'normal'}}>vote in the most recent proposal?</span>
             </div>
           </div>
         ),
@@ -171,44 +176,49 @@ try {
     throw new Error('Invalid type returned');
   }
     return c.res({
-        image: (          
-          <div style={{
-            display: 'flex',
-            background: '#f6f6f6',
-            width: '100%',
-            height: '100%',
-            flexDirection: 'column',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            position: 'relative'
-            }}>
-            <img width="1200" height="630" alt="background" src={`/Frame_2_stats.png`} />
-            <div
-              style={{
-                position: 'absolute',
-                color: '#E5383B',
-                fontSize: '75px',
-                lineHeight: '0.7',
-                textTransform: 'uppercase',
-                letterSpacing: '-0.030em',
-                whiteSpace: 'wrap',
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-                with: '100%',
-                maxWidth: '240px',
-                height: '100%',
-                maxHeight: '340px',
-                left: '195px',
-                bottom: '230px',
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-              }}>
-              {`${delegateData}`}
-            </div>
+      image: (
+        <div style={{
+          display: 'flex',
+          background: '#f6f6f6',
+          width: '100%',
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          position: 'relative'
+        }}>
+          {/* @ts-ignore */}
+          <img width="1200" height="630" alt="background" src={`/Frame_2_stats_dynamic.png`} style={{position: 'absolute', width: '100%', height: '100%', objectFit: 'cover'}} />
+          <div
+            style={{
+              display: 'flex',
+              position: 'absolute',
+              color: '#000000', 
+              fontSize: '75px', 
+              fontWeight: 'bold', 
+              lineHeight: '0.7', 
+              textTransform: 'uppercase', 
+              letterSpacing: '-0.030em',
+              whiteSpace: 'wrap',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              width: '100%', 
+              margin: '20px', 
+              paddingLeft: '25px',
+              paddingRight: '25px',
+              left: '-20px', 
+              top: '20px', 
+              textAlign: 'center', 
+            }}
+          >    
+            {`Did `}
+            <span style={{ color: '#E5383B' }}>{delegateUpperCase}</span>
+            <span style={{ width: '100%', wordWrap: 'break-word', whiteSpace: 'normal'}}>vote in the most recent proposal?</span>
           </div>
-        ),
+        </div>
+      ),
         intents: [
-          ,
+          <Button.Link href='https://warpcast.com/lauraocampo'>Share</Button.Link>,
           <Button.Reset>Reset</Button.Reset>
         ],
       })
@@ -228,153 +238,138 @@ function getIntents(delegates: addressCount[]) : FrameIntent[]{
   })
 }
 
+function truncateMiddle (text: string, maxLength: number) : string{
+  if (text.length <= maxLength) return text
+  const start = Math.ceil((maxLength - 3) / 2)
+  const end = Math.floor((maxLength - 3) / 2)
+  return text.slice(0, start) + '...' + text.slice(-end)
+}
+
 app.frame('/exploreDelegates', async (c) => {
-  const {  frameData } = c;
-  const { fid } = frameData || {}   
+ /* const {  frameData } = c;
+ const { fid } = frameData || {} */
+
+ const fid = 192336
+
 
   if (typeof fid !== 'number' || fid === null) {
     return c.res({
       image: `/Frame_6_error.png`,
       imageAspectRatio: '1.91:1',
-      intents: [
-        <Button.Reset>Try again</Button.Reset>,
-      ],
+      intents: [<Button.Reset>Try again</Button.Reset>],
     });
   }
+  let delegates: suggestionResponseDTO 
 
-  let delegates: suggestionResponseDTO
-  let intents: FrameIntent[]
-
-  try{
+  try {
     const delegateApiURL = new URL(`${process.env.DELEGATE_API_URL}/get_suggested_delegates`);
     delegateApiURL.searchParams.append('fid', fid.toString());
 
-    const response = await fetch(delegateApiURL, {
+    const response = await fetch(delegateApiURL.toString(), {
       method: 'GET',
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  })
-
-  if (!response.ok){
-    throw new Error(`Error get delegate info for fid ${fid}`)
-  }
-
-  delegates = await response.json();
-  if (delegates.length === 0) {
-    return c.res({
-      image: `/back2.png`,
-      imageAspectRatio: '1.91:1',
-      intents: [
-        <Button.Reset>Try again</Button.Reset>,
-      ],
+      headers: { 'Content-Type': 'application/json' },
     });
-  }
 
-  intents = getIntents(delegates)
-  intents.push(<Button.Reset>Reset</Button.Reset>)
-  
-  } catch (e) {
-    console.error('Error fetching delegate data:', e);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-    return c.res({
-      image: `/Frame_6_error.png`,
-      imageAspectRatio: '1.91:1',
-      intents: [
-        <Button.Reset>Try again</Button.Reset>,
-      ],
-    });
-  }
+    delegates = await response.json();
 
-
+} catch (error) {
+  console.error('Error fetching delegate data:', error);
   return c.res({
-    image: (
-      <div style={{
-        display: 'flex',
-        background: '#f6f6f6',
-        width: '100%',
-        height: '100%',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        position: 'relative'
-      }}>
-        <img width="1200" height="630" alt="background" src={`/Frame_3_rec.png`}/>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            position: 'absolute',
-            color: '#161B33',
-            fontSize: '70px',
-            textTransform: 'uppercase',
-            letterSpacing: '-0.030em',
-            width: '100%',
-            height: '100%',
-            justifyContent: 'center',
-            boxSizing: 'border-box',
-            alignItems: 'center',
-            lineHeight: 1.4,
-            padding: '0px 50px',
-            overflow: 'hidden', 
-            textOverflow: 'ellipsis',
-            textAlign: 'center', 
-          }}>
-            <h1></h1>
-            <div style={{
-                display: 'flex',
-                flexDirection: 'row',
-                width: '100%',
-                maxWidth: '100%',
-                justifyContent: 'center',
-                flexWrap: 'wrap',
-                gap: '10px'
-            }}>
-                  <ul style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    listStyleType: 'none',
-                    padding: '0',
-                    margin: '0',
-                    width: '40%',
-                    boxSizing: 'border-box'
-                  }}>
-                    {delegates.map((item, index) => (
-                      <li key={index} style={{
-                        margin: '10px 0',
-                        padding: '5px',
-                        borderBottom: '1px solid #ddd',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap',
-                      }}>{item.address}</li>
-                    ))}
-                  </ul>
-                
-                  <ul style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    listStyleType: 'none',
-                    padding: 0,
-                    margin: '0',
-                    width: '20%',
-                    boxSizing: 'border-box',
-                  }}>
-                    {delegates.map((item, index) => (
-                      <li key={index} style={{ margin: '10px 0',
-                        padding: '5px',
-                        borderBottom: '1px solid #ddd',
-                        justifyContent: 'flex-end', }}>{item.count}</li>
-                    ))}
-                  </ul>
-            </div>
-        </div>
-      </div>
-    ),
-    intents: intents
+    image: `/Frame_6_error.png`,
+    imageAspectRatio: '1.91:1',
+    intents: [<Button.Reset>Try again</Button.Reset>],
   });
-})
+}
+if (delegates.length === 0) {
+  return c.res({
+    image: `/back2.png`,
+    imageAspectRatio: '1.91:1',
+    intents: [<Button.Reset>Try again</Button.Reset>],
+  });
+}
 
+const intents = getIntents(delegates);
+intents.push(<Button.Reset>Reset</Button.Reset>);
+
+return c.res({
+image: (  
+<div
+  style={{
+    display: 'flex',
+    background: '#f6f6f6',
+    alignItems: 'center',
+    position: 'relative',
+  }}
+> 
+  <img width="1200" height="630" alt="background" src={`/Frame_3.png`} />
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      position: 'absolute',
+      color: '#161B33',
+      fontSize: '65px',
+      textTransform: 'uppercase',
+      letterSpacing: '-0.030em',
+      width: '100%',
+      boxSizing: 'border-box',
+      alignItems: 'center',
+      lineHeight: 0.8,
+      padding: '0px 50px',
+      overflow: 'hidden', 
+      textOverflow: 'ellipsis',
+      textAlign: 'center', 
+      top: '20%',
+      height: '80%',
+    }}>      
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row', // Alinea los ul en una fila
+      flexWrap: 'wrap', // Permite que los ul se envuelvan en múltiples líneas si es necesario
+      width: '100%',
+      maxWidth: '100%',
+      justifyContent: 'center',
+    }}>
+      {[0, 1, 2].map(colIndex => (
+        <div key={colIndex} style={{
+          display: 'flex',
+          flexDirection: 'column', // Coloca los items en una columna
+          width: '30%', // Ajusta el ancho para tres columnas
+          boxSizing: 'border-box',
+          margin: '0 10px', // Espacio entre columnas
+        }}>
+          {delegates
+            .filter((_, index) => index % 3 === colIndex) // Filtra los elementos para la columna actual
+            .map((item, index) => (
+              <div key={index} style={{
+                display: 'flex',
+                flexDirection: 'column', // Coloca address y count en una columna
+                margin: '5px 0',
+                alignItems: 'center',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap',
+                height: 'auto', // Ajusta la altura según el contenido  
+              }}>                    
+                {truncateMiddle(item.address, 11)}     
+                <br/>
+                {item.count}
+              </div>
+            ))
+          }
+        </div>
+      ))}
+    </div>
+  </div>
+</div>
+),
+intents,
+});
+
+})
 
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== 'undefined'
