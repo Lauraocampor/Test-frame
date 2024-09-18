70418959d61356fce28701ec60da679f58b4e7a7
Laura Ocampo Rodríguez <92342585+Lauraocampor@users.noreply.github.com>
2024-09-18 15:19:56 -0400

    styles on good delegate frame added

--- api/index.tsx
+++ api/index.tsx
@@ -42,6 +42,13 @@ app.frame('/', (c) => {
   })
 })
 
+function truncateMiddle (text: string, maxLength: number) : string{
+  if (text.length <= maxLength) return text
+  const start = Math.ceil((maxLength - 3) / 2)
+  const end = Math.floor((maxLength - 3) / 2)
+  return text.slice(0, start) + '...' + text.slice(-end)
+}
+
 app.frame('/delegatesStats', async (c) => {
  /* const {  frameData } = c;
  const { fid } = frameData || {} */
@@ -122,12 +129,10 @@ try {
   }
 
   const userDelegate = delegate.delegateInfo.warpcast
-  const addressDelegate = delegate.delegateInfo.delegateAddress
+  const addressDelegate = truncateMiddle(delegate.delegateInfo.delegateAddress, 11)
 
-  const delegateData = userDelegate? userDelegate : addressDelegate
+  const delegateData =  addressDelegate
   const delegateUpperCase= delegateData.toUpperCase()
-
-  delegate.isGoodDelegate = false
 
   /* BAD DELEGATE FRAME */
 
@@ -191,48 +196,49 @@ try {
   }
 
   /* GOOD DELEGATE FRAME */
-    return c.res({
-      image: (
-        <div style={{
-          display: 'flex',
-          background: '#f6f6f6',
-          width: '100%',
-          height: '100%',
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-          alignItems: 'center',
-          position: 'relative'
-        }}>
-          {/* @ts-ignore */}
-          <img width="1200" height="630" alt="background" src={`/Frame_2_stats_dynamic.png`} style={{position: 'absolute', width: '100%', height: '100%', objectFit: 'cover'}} />
-          <div
-            style={{
-              display: 'flex',
-              position: 'absolute',
-              color: '#000000', 
-              fontSize: '75px', 
-              fontWeight: 'bold', 
-              lineHeight: '0.7', 
-              textTransform: 'uppercase', 
-              letterSpacing: '-0.030em',
-              whiteSpace: 'wrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              width: '100%', 
-              margin: '20px', 
-              paddingLeft: '25px',
-              paddingRight: '25px',
-              left: '-20px', 
-              top: '20px', 
-              textAlign: 'center', 
-            }}
-          >    
-            {`Did `}
-            <span style={{ color: '#E5383B' }}>{delegateUpperCase}</span>
-            <span style={{ width: '100%', wordWrap: 'break-word', whiteSpace: 'normal'}}>vote in the most recent proposal?</span>
-          </div>
+  return c.res({
+    image: (
+      <div style={{
+        display: 'flex',
+        background: '#f6f6f6',
+        width: '100%',
+        height: '100%',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        position: 'relative'
+      }}>
+        {/* @ts-ignore */}
+        <img width="1200" height="630" alt="background" src={`/Frame_2_stats_dynamic.png`} style={{position: 'absolute', width: '100%', height: '100%', objectFit: 'cover'}} />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'absolute',
+            color: '#161B33',
+            fontSize: '65px',
+            textTransform: 'uppercase',
+            letterSpacing: '-0.030em',
+            width: '100%',
+            maxWidth: '100%',
+            boxSizing: 'border-box',
+            alignItems: 'flex-start',
+            lineHeight: 0.8,
+            padding: '10px',
+            overflow: 'hidden', 
+            textOverflow: 'ellipsis',
+            textAlign: 'center', 
+            top: '3%',
+            height: '30%',
+            lineClamp: 2,
+            whiteSpace: 'wrap'
+          }}
+        >            
+        <div style={{display: 'flex', wordWrap: 'break-word', lineClamp: 2,  flexWrap: 'wrap', width: '100%',
+  maxWidth: '100%', margin: '0 10px', justifyContent: 'center',}}>Did <div style={{display: 'flex', color: '#E5383B'}}>{delegateUpperCase}</div> vote in the most recent proposal?</div>
         </div>
-      ),
+      </div>
+    ),
         intents: [
           <Button.Link href='https://warpcast.com/lauraocampo'>Share</Button.Link>,
           <Button.Reset>Reset</Button.Reset>
@@ -252,13 +258,6 @@ function getIntents(delegates: addressCount[]) : FrameIntent[]{
     const position = index+1
     return <Button.Link href={`https://vote.optimism.io/delegates/${delegate.address}`}>{`${position}${getOrdinalSuffix(position)} Delegate`}</Button.Link>
   })
-}
-
-function truncateMiddle (text: string, maxLength: number) : string{
-  if (text.length <= maxLength) return text
-  const start = Math.ceil((maxLength - 3) / 2)
-  const end = Math.floor((maxLength - 3) / 2)
-  return text.slice(0, start) + '...' + text.slice(-end)
 }
 
 app.frame('/socialRecommendation', async (c) => {
